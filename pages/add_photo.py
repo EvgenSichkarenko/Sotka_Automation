72ca7fc5de3b7dd67fb33b7f97ef9f89df56b4da
EvgenSichkarenko <sichkarenko96@gmail.com>
2022-04-19 17:19:06 +0300

    add new path photo

--- pages/add_photo.py
+++ pages/add_photo.py
@@ -10,16 +10,16 @@ class Photo:
 
 	def add_photo(self):
 		wd = self.app.wd
-		image = os.path.abspath("./data/images/logo.jpg")
+		image = os.path.abspath("/data/images/logo.jpg")
 		#image = os.path.abspath("/var/lib/jenkins/workspace/Test_sotka/data/images/logo.jpg")
 
 		WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.ID, "basic-button"))).click()
 		wd.find_element(By.XPATH, "//div[text()=' Change photo']").click()
 
-		title = wd.find_element(By.XPATH, "//h1[text()='Your photo account']").text
+		title = wd.find_element(By.XPATH, "//h1[text()='Your photo account']").get_attribute("textContent")
 
 		if title == "Your photo account":
-			wd.find_element(By.NAME, "uploadPhotoRef").send_keys(image)
+			wd.find_element(By.CSS_SELECTOR, "input[name='uploadPhotoRef']").send_keys(image)
 			wd.find_element(By.NAME, "uploadPhotoSaveBtn").click()
 			return True
 		else:
