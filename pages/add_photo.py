41a5cdeec64b5292d03299bd836ff4db5a5f514a
EvgenSichkarenko <sichkarenko96@gmail.com>
2022-04-20 12:38:23 +0300

    new path to photo

--- pages/add_photo.py
+++ pages/add_photo.py
@@ -10,7 +10,7 @@ class Photo:
 
 	def add_photo(self):
 		wd = self.app.wd
-		image = os.path.abspath("data/images/logo")
+
 		#image = os.path.abspath("C:\Python_project\Sotka_auto\data\images\logo.jpg")
 		basic_btn = WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div[id='basic-button']")))
 		wd.execute_script("arguments[0].click();", basic_btn)
@@ -20,7 +20,8 @@ class Photo:
 
 		if title == "Your photo account":
 			time.sleep(1)
-			wd.find_element(By.CSS_SELECTOR, "input[name='uploadPhotoRef']").send_keys(os.path.abspath("data/images/logo"))
+			image = os.path.abspath("data/images/logo")
+			wd.find_element(By.CSS_SELECTOR, "input[name='uploadPhotoRef']").send_keys(image)
 			wd.find_element(By.NAME, "uploadPhotoSaveBtn").click()
 			return True
 		else:
