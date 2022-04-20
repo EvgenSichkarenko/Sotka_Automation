95f117eaaa50b3e82c0fd76f58593903d9edd961
EvgenSichkarenko <sichkarenko96@gmail.com>
2022-04-20 14:01:25 +0300

    new path

--- pages/add_photo.py
+++ pages/add_photo.py
@@ -10,8 +10,8 @@ class Photo:
 
 	def add_photo(self):
 		wd = self.app.wd
-		#image = os.path.abspath("/var/lib/jenkins/workspace/Test_stoke_sotka/data/images/logo.jpg")
-		image = os.path.abspath("~/data/images/logo.jpg")
+		image = os.path.abspath("/var/lib/jenkins/workspace/Test_stoke_sotka/data/images/logo.jpg")
+		#image = os.path.abspath("~/data/images/logo.jpg")
 		#image = os.path.abspath("C:\Python_project\Sotka_auto\data\images\logo.jpg")
 		basic_btn = WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div[id='basic-button']")))
 		wd.execute_script("arguments[0].click();", basic_btn)
