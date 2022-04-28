10ca559b28175bb6552965d2bb79bfb153c9c78a
EvgenSichkarenko <sichkarenko96@gmail.com>
2022-04-28 17:48:05 +0300

    add new path to finish

--- pages/deposition_case.py
+++ pages/deposition_case.py
@@ -530,11 +530,11 @@ class DepositionCase:
 		assert phone_op == f"{op_phone}"
 
 		#Cr info
-		wd.refresh()
+		#wd.refresh()
 		time.sleep(3)
 		cr = wd.find_element(By.CSS_SELECTOR, "div[data-name='finishExecutorBlock']")
-		time.sleep(1)
-		name_cr =  cr.find_element(By.XPATH, f"//h2[text()='{cr_name}']").get_attribute("textContent")
+		name_cr = WebDriverWait(cr, 15).until(EC.element_to_be_clickable((By.XPATH, f"//h2[text()='{cr_name}']"))).text
+		#name_cr =  cr.find_element(By.XPATH, f"//h2[text()='{cr_name}']").get_attribute("textContent")
 		email_cr = cr.find_element(By.XPATH, f"//span[text()='{cr_email}']").text
 		phone_cr = cr.find_element(By.XPATH,f"//span[text()='{cr_phone}']").text
 
